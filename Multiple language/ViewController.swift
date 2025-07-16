c5812cb54352dc32bb9657d0dbff41588058b85b
Rohit <appledevelopment12@gmail.com>
2025-07-16 13:47:59 +0530

    Initial Commit

---
+++ Multiple language/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multiple language
+//
+//  Created by Rohit on 16/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
